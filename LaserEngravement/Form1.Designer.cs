07e16f2598f87c50c727f0ddaaf5a1690de6293f
Jonyap997 <jonyap997@gmail.com>
2019-09-26 23:13:54 +0800

    Created windows form

---
+++ LaserEngravement/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LaserEngravement
+{
+    partial class LaserEngravementProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUploadImage = new System.Windows.Forms.Label();
+            this.btnUploadImage = new System.Windows.Forms.Button();
+            this.txtUploadImage = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.picSampleImage = new System.Windows.Forms.PictureBox();
+            this.btnEngrave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEmergency = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picSampleImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(43, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laser engravement program";
+            // 
+            // lblUploadImage
+            // 
+            this.lblUploadImage.AutoSize = true;
+            this.lblUploadImage.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUploadImage.Location = new System.Drawing.Point(49, 154);
+            this.lblUploadImage.Name = "lblUploadImage";
+            this.lblUploadImage.Size = new System.Drawing.Size(111, 23);
+            this.lblUploadImage.TabIndex = 1;
+            this.lblUploadImage.Text = "Upload image:";
+            // 
+            // btnUploadImage
+            // 
+            this.btnUploadImage.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnUploadImage.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnUploadImage.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUploadImage.Location = new System.Drawing.Point(430, 155);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size(35, 23);
+            this.btnUploadImage.TabIndex = 3;
+            this.btnUploadImage.Text = "...";
+            this.btnUploadImage.UseVisualStyleBackColor = false;
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+            // 
+            // txtUploadImage
+            // 
+            this.txtUploadImage.Location = new System.Drawing.Point(166, 156);
+            this.txtUploadImage.Name = "txtUploadImage";
+            this.txtUploadImage.ReadOnly = true;
+            this.txtUploadImage.Size = new System.Drawing.Size(258, 22);
+            this.txtUploadImage.TabIndex = 2;
+            this.txtUploadImage.TextChanged += new System.EventHandler(this.txtUploadImage_TextChanged);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(49, 199);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(114, 23);
+            this.lbl.TabIndex = 4;
+            this.lbl.Text = "Sample image:";
+            this.lbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // picSampleImage
+            // 
+            this.picSampleImage.Location = new System.Drawing.Point(169, 199);
+            this.picSampleImage.Name = "picSampleImage";
+            this.picSampleImage.Size = new System.Drawing.Size(200, 100);
+            this.picSampleImage.TabIndex = 5;
+            this.picSampleImage.TabStop = false;
+            // 
+            // btnEngrave
+            // 
+            this.btnEngrave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnEngrave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnEngrave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEngrave.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEngrave.Location = new System.Drawing.Point(166, 337);
+            this.btnEngrave.Name = "btnEngrave";
+            this.btnEngrave.Size = new System.Drawing.Size(80, 35);
+            this.btnEngrave.TabIndex = 6;
+            this.btnEngrave.Text = "Engrave";
+            this.btnEngrave.UseVisualStyleBackColor = true;
+            this.btnEngrave.Click += new System.EventHandler(this.btnEngrave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(166, 389);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 35);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEmergency
+            // 
+            this.btnEmergency.BackColor = System.Drawing.Color.Red;
+            this.btnEmergency.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnEmergency.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnEmergency.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmergency.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmergency.ForeColor = System.Drawing.Color.FloralWhite;
+            this.btnEmergency.Location = new System.Drawing.Point(259, 337);
+            this.btnEmergency.Name = "btnEmergency";
+            this.btnEmergency.Size = new System.Drawing.Size(110, 87);
+            this.btnEmergency.TabIndex = 8;
+            this.btnEmergency.Text = "Emergency Kill Switch";
+            this.btnEmergency.UseVisualStyleBackColor = false;
+            // 
+            // LaserEngravementProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.btnEmergency);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEngrave);
+            this.Controls.Add(this.picSampleImage);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btnUploadImage);
+            this.Controls.Add(this.txtUploadImage);
+            this.Controls.Add(this.lblUploadImage);
+            this.Controls.Add(this.label1);
+            this.Name = "LaserEngravementProgram";
+            this.Text = "Laser Engravement Program";
+            ((System.ComponentModel.ISupportInitialize)(this.picSampleImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUploadImage;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.TextBox txtUploadImage;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.PictureBox picSampleImage;
+        private System.Windows.Forms.Button btnEngrave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEmergency;
+    }
+}
+
---
+++ LaserEngravement/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaserEngravement
+{
+    public partial class LaserEngravementProgram : Form
+    {
+        public LaserEngravementProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog uploadImage = new OpenFileDialog();
+
+            uploadImage.InitialDirectory = @"C:\";
+            uploadImage.RestoreDirectory = true;
+            uploadImage.Title = "Upload Image File";
+            uploadImage.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            uploadImage.CheckFileExists = true;
+            uploadImage.CheckPathExists = true;
+            uploadImage.Multiselect = false;
+
+            if (uploadImage.ShowDialog() == DialogResult.OK)
+            {
+                txtUploadImage.Text = uploadImage.FileName;
+                
+            }
+
+        }
+
+        private void txtUploadImage_TextChanged(object sender, EventArgs e)
+        {
+            string resizedImagePath = "resized_" + txtUploadImage.Text;
+
+            System.Drawing.Image img = System.Drawing.Image.FromFile(txtUploadImage.Text);
+            img = Resize(img, 200, 100);
+            img.Save(resizedImagePath);
+
+            picSampleImage.Image = new Bitmap(resizedImagePath);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEngrave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //resize image 
+        private Image Resize(Image img, int iWidth, int iHeight)
+        {
+            Bitmap bmp = new Bitmap(iWidth, iHeight);
+            Graphics graphic = Graphics.FromImage((Image)bmp);
+            graphic.DrawImage(img, 0, 0, iWidth, iHeight);
+
+            return (Image)bmp;
+        }
+    }
+}
